a990bfe17ad77f34a2d4e8e7a49a4335ccd49392
harsha <121601031+harshabavigadda@users.noreply.github.com>
2025-04-15 11:33:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ const BetCalculator = () => {
     const payoutTeamA = amountTeamA * oddsTeamA;
     const payoutTeamB = amountTeamB * oddsTeamB;
     const loss = totalUSDT - (payoutTeamA + payoutTeamB)/2;
-    const profit = payoutTeamA + payoutTeamB - totalUSDT;
-    //const pinr = profit*85,
+    let profit = payoutTeamA + payoutTeamB - totalUSDT;
+    let pinr = profit * 85,
     return {
       amountTeamA,
       amountTeamB,
@@ -24,7 +24,7 @@ const BetCalculator = () => {
       payoutTeamB,
       loss: loss.toFixed(2),
       profit: profit.toFixed(3),
-     // pinr,
+      pinr,
     };
   };
 
@@ -89,7 +89,7 @@ const BetCalculator = () => {
           <div className="mt-4">
             <p><strong>Total Investment:</strong> 23.5 USDT (≈ ₹2000)</p>
             <p><strong>Minimum_Loss (approx):</strong> {result.loss} USDT</p>
-            //<p><strong>Max Profit (approx):</strong> { result.profit } USDT ≈ ₹{result.pinr}</p>
+            <p><strong>Max Profit (approx):</strong> { result.profit } USDT ≈ ₹{result.pinr}</p>
           </div>
         </div>
       )}
